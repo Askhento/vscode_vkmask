91d4742ef324483db8c4fb4309d4553a296a6121
askhento <mudarisowash@gmail.com>
2024-01-04 00:50:13 +0500

    fixed dynamic require sharp

--- rollup.extension.mjs
+++ rollup.extension.mjs
@@ -3,20 +3,24 @@ import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 // import esbuild from "rollup-plugin-esbuild";
 import typescript from "@rollup/plugin-typescript"; // esbuild have issues with svelte
+import json from "@rollup/plugin-json"
 
-// import json from "@rollup/plugin-json";
-
-// for config autocomplete
 /** @type {import("rollup").RollupOptions} */
 export const extensionConfig = {
     input: "src/extension.ts",
-    external: ["vscode", "sharp"],
+    external: ["vscode"],
     output: {
         sourcemap: true,
         format: "cjs",
         file: "./out/extension.js",
     },
-    plugins: [nodeResolve(), commonjs({}), typescript({})], // json()
+    plugins: [
+        nodeResolve({
+        }),
+        commonjs({
+            ignoreDynamicRequires: true
+        }),
+        typescript({}), json()],
     watch: {
         clearScreen: false,
         skipWrite: false,
