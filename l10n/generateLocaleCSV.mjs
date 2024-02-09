69ccb17c683de8a096eb6d9f5259364d2ec4880b
Askhento <mudarisowash@gmail.com>
2024-02-09 05:23:03 +0500

    add locale en ru generated csv

--- l10n/generateLocaleCSV.mjs
+++ l10n/generateLocaleCSV.mjs
@@ -6,32 +6,35 @@ import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const localeBundles = ["./l10n/bundle.l10n.ru.json", "./package.nls.ru.json"];
+function generateCSV(bundles, locale) {
+    const localeObject = {};
 
-const localeObject = {};
-
-localeBundles.forEach((bundleName) => {
-    // const bundlePath = path.join(__dirname, bundleName);
-    const bundle = JSON.parse(fs.readFileSync(bundleName));
-    Object.keys(bundle).forEach((key) => {
-        if (key in localeObject) {
-            console.log(`Duplicate key ${key} in ${bundleName}`);
-            return;
-        }
-        localeObject[key] = bundle[key];
+    bundles.forEach((bundleName) => {
+        // const bundlePath = path.join(__dirname, bundleName);
+        const bundle = JSON.parse(fs.readFileSync(bundleName));
+        Object.keys(bundle).forEach((key) => {
+            if (key in localeObject) {
+                console.log(`Duplicate key ${key} in ${bundleName}`);
+                return;
+            }
+            localeObject[key] = bundle[key];
+        });
     });
-});
 
-const sortedTranslations = Object.entries(localeObject).sort(([keya, vala], [keyb, valb]) => {
-    return keya.localeCompare(keyb);
-});
+    const sortedTranslations = Object.entries(localeObject).sort(([keya, vala], [keyb, valb]) => {
+        return keya.localeCompare(keyb);
+    });
 
-// console.log(sortedTranslations);
+    // console.log(sortedTranslations);
 
-let result = "";
+    let result = "";
 
-sortedTranslations.forEach(([key, value]) => {
-    result += `${key};"${value}"\n`;
-});
+    sortedTranslations.forEach(([key, value]) => {
+        result += `${key};"${value}"\n`;
+    });
 
-fs.writeFileSync("./l10n/generated.csv", result, { encoding: "utf8" });
+    fs.writeFileSync(`./l10n/generated_${locale}.csv`, result, { encoding: "utf8" });
+}
+
+generateCSV(["./l10n/bundle.l10n.ru.json", "./package.nls.ru.json"], "ru");
+generateCSV(["./l10n/bundle.l10n.json", "./package.nls.json"], "en");
