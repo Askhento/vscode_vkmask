c77266bae3401f91e0faf22de13122b1fd8354da
askhento <mudarisowash@gmail.com>
2024-05-18 00:20:52 +0500

    add package.sh script

---
+++ package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# "package-win32-x64": "npx vsce package --target win32-x64 --ignoreFile .win32-x64.vscodeignore --out ./vsix-packages",
+# "package-darwin-x64": "npx vsce package --target darwin-x64 --ignoreFile .darwin-x64.vscodeignore --out ./vsix-packages",
+# "package-darwin-arm64": "npx vsce package --target darwin-arm64 --ignoreFile .darwin-arm64.vscodeignore --out ./vsix-packages",
+    
+platforms=$@
+
+function packExt {
+    ignoreFile=".$1.vscodeignore"
+    if [[ $PRE_RELEASE ]]; then
+        npx vsce package --target "$1" --ignoreFile "$ignoreFile" --out ./vsix-packages
+     else
+        npx vsce package --pre-release --target "$1" --ignoreFile "$ignoreFile" --out ./vsix-packages
+     fi
+}
+
+for p in $platforms;
+do
+    packExt $p
+done
