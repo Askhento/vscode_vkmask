0c7d86b5cfb958389d1565f933e6b15dea09ece6
askhento <mudarisowash@gmail.com>
2024-05-19 05:17:55 +0500

    no words

--- publish.sh
+++ publish.sh
@@ -5,9 +5,9 @@ publish=$1
 export RELEASE=$(node -e "const {version} = require('./package.json'); console.log(version.split('.')[1] % 2 == 0)")
 echo "version is release: ${RELEASE}"
 
-# npm run generateIgnoreFiles
+npm run generateIgnoreFiles
 
-# npm run package-all
+npm run package-all
 
 cmd=(--packagePath $(find ./vsix-packages -iname *.vsix))
 
